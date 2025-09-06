55a0eb1a9030e509883a77e6dcce96054a633e38
Nyukiland <roman211113@gmail.com>
2025-09-06 23:24:23 +0200

    [UPD] PhysicResource - Force pipeline in progress

--- Assets/Script/plateformer/PhysicResource.cs
+++ Assets/Script/plateformer/PhysicResource.cs
@@ -1,10 +1,9 @@
-using Modules.CustomAttribute;
-using Modules.DevLoggers;
-using Modules.StateMachine;
-using System;
-using System.Collections;
 using System.Collections.Generic;
+using Modules.CustomAttribute;
+using Modules.StateMachine;
+using Modules.DevLoggers;
 using UnityEngine;
+using System;
 
 public class PhysicResource : Resource
 {
@@ -18,7 +17,75 @@ public class PhysicResource : Resource
 
 	[SerializeField]
 	[Disable]
+	private Vector3 _currentVelo;
+
+	[SerializeField]
+	[Disable]
+	private Vector3? _globalVelo;
+
+	[SerializeField]
+	[Disable]
 	private List<ForceIdentifier> _forceToProcess;
+
+	public void AddForce(Vector3 force) => 
+		AddForce(new ForceIdentifier(force));
+	public void AddForce(Vector3 force, float timeRemove, AnimationCurve curveRemove = null) => 
+		AddForce(new ForceIdentifier(force, timeRemove, curveRemove));
+	public void AddForce(Vector3 force, float timeAdd, float timeRemove, AnimationCurve curveAdd = null, AnimationCurve curveRemove = null) => 
+		AddForce(new ForceIdentifier(force, timeAdd, timeRemove, curveAdd, curveRemove));
+
+	private void AddForce(ForceIdentifier force)
+	{
+		DevLogger.Log($"Add: \n" +
+			$"Force: {force.Force} \n" +
+			$"Time Add: {force.TimeToAdd} \n" +
+			$"Time Remove: {force.TimeToRemove}",
+			nameof(PhysicResource));
+
+		_forceToProcess.Add(force);
+	}
+
+	public void SetVelocity(Vector3 velocity)
+	{
+		_currentVelo = velocity;
+	}
+
+	public void SetGlobalVelocity(Vector3 velocity)
+	{
+		_globalVelo = velocity;
+	}
+
+	public void StopAllForce() => SetGlobalVelocity(Vector3.zero);
+
+	public void ClearAllForce() => _forceToProcess.Clear();
+
+	public void StopAndClearAllForce()
+	{
+		ClearAllForce();
+		StopAllForce();
+	}
+
+	public override void FixedUpdate(float fixedDeltaTime)
+	{
+		base.FixedUpdate(fixedDeltaTime);
+
+		Vector3 allForces = Vector3.zero;
+
+		for (int i = _forceToProcess.Count - 1; i >= 0; i--)
+		{
+			_forceToProcess[i].UpdateForce(fixedDeltaTime);
+			allForces += _forceToProcess[i].GetForce();
+
+			if (_forceToProcess[i].IsForceComplete())
+			{
+				DevLogger.Log($"Force Removed \n" +
+					$"Force: {_forceToProcess[i].Force}", 
+					nameof(PhysicResource));
+
+				_forceToProcess.RemoveAt(i);
+			}
+		}
+	}
 }
 
 [Serializable]
@@ -39,24 +106,11 @@ public class ForceIdentifier
 		Force = force;
 	}
 
-	public ForceIdentifier(Vector3 force, float timeRemove)
-	{
-		Force = force;
-		TimeToRemove = timeRemove;
-	}
-
 	public ForceIdentifier(Vector3 force, float timeRemove, AnimationCurve curveRemove)
 	{
 		Force = force;
 		TimeToRemove = timeRemove;
 		RemoveForceCurve = curveRemove;
-	}
-
-	public ForceIdentifier(Vector3 force, float timeAdd, float timeRemove)
-	{
-		Force = force;
-		TimeToRemove = timeRemove;
-		TimeToAdd = timeAdd;
 	}
 
 	public ForceIdentifier(Vector3 force, float timeAdd, float timeRemove, AnimationCurve curveAdd, AnimationCurve curveRemove)
@@ -66,5 +120,20 @@ public class ForceIdentifier
 		TimeToAdd = timeAdd;
 		AddForceCurve = curveAdd;
 		RemoveForceCurve = curveRemove;
+	}
+
+	public void UpdateForce(float deltaTime)
+	{
+
+	}
+
+	public Vector3 GetForce()
+	{
+		return Force;
+	}
+
+	public bool IsForceComplete()
+	{
+		return _internalTimer >= TimeToRemove + TimeToAdd;
 	}
 }
